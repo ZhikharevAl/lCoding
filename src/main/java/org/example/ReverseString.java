63829f99c30b1bd796bb3f68d60c8238742b9171
ZhikharevAl <waltafunk@gmail.com>
2026-04-23 22:29:07 +0300

    feat: added reverse string

--- src/main/java/org/example/ReverseString.java
+++ src/main/java/org/example/ReverseString.java
@@ -1,4 +1,18 @@
 package org.example;
 
+/*
+Напишите функцию, которая переворачивает строку. Входная строка задана как массив символов s.
+
+Это необходимо сделать, изменив входной массив на месте с использованием O(1) дополнительной памяти. Не нужно возвращать исходный массив!
+*/
+
 public class ReverseString {
+    public void reverseString(char[] s) {
+        char temp;
+        for (int i = 0; i < s.length / 2; i++) {
+            temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - i - 1] = temp;
+        }
+    }
 }
